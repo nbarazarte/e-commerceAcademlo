50dcb2a8fa7d6e7187d5fccb84c5fa718493f312
Neel <ezebarazarte@gmail.com>
2023-03-10 21:40:56 -0400

    empezando a consumir la api

--- assets/main.js
+++ assets/main.js
@@ -25,3 +25,28 @@ $(function(){
       }
     });
   });
+
+async function getProducts(){
+
+  try {
+    const data = await fetch("https://ecommercebackend.fundamentos-29.repl.co/");
+
+    const res = await data.json();
+
+    window.localStorage.setItem('products', JSON.stringify(res));
+
+    //console.log(res);
+    return res;
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+(async () => {
+  const res = JSON.parse(window.localStorage.getItem('products')) || await getProducts();
+
+  console.log(res);
+
+
+})();
